ab1434b49152b0e8393f8a98d2fff9cfbfe3767c
mdquang25 <mdquang25@gmail.com>
2024-02-26 11:42:40 +0700

    try 2

--- src/routes/middleware/uploader.js
+++ src/routes/middleware/uploader.js
@@ -2,10 +2,9 @@ const multer = require('multer');
 const path = require('path');
 // Configure the destination and filename for uploaded images
 const storage = multer.diskStorage({
-    // destination: function (req, file, cb) {
-    //     cb(null, 'src/public/uploads/');
-    // },
-    destination: path.join(__dirname, 'src', 'public', 'uploads'),
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, 'src', 'public', 'uploads'));
+    },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname);
     }
