1d3546ecac8fda54b5691b2f97e4ef340b7e4a7e
mdquang25 <mdquang25@gmail.com>
2024-02-26 14:55:29 +0700

    untracked uploads/*

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        //await mongoose.connect('mongodb://mongo:hEfAee3Cg-FeeD2HcafE45FDHFaaGB-d@monorail.proxy.rlwy.net:40488');///*'mongodb://localhost:27017/baitaplon1_db_dev');*/'mongodb+srv://mdquang25_mongodb:ENA7RghS0Q2yfL0J@cluster0.twexh1b.mongodb.net/baitaplon1_db_dev?retryWrites=true&w=majority');
-        await mongoose.connect('mongodb://mongo:hEfAee3Cg-FeeD2HcafE45FDHFaaGB-d@mongodb-uhyx.railway.internal:27017');
+        await mongoose.connect('mongodb://mongo:hEfAee3Cg-FeeD2HcafE45FDHFaaGB-d@monorail.proxy.rlwy.net:40488');///*'mongodb://localhost:27017/baitaplon1_db_dev');*/'mongodb+srv://mdquang25_mongodb:ENA7RghS0Q2yfL0J@cluster0.twexh1b.mongodb.net/baitaplon1_db_dev?retryWrites=true&w=majority');
+        //await mongoose.connect('mongodb://mongo:hEfAee3Cg-FeeD2HcafE45FDHFaaGB-d@mongodb-uhyx.railway.internal:27017');
         console.log('connect successfully!');
     } catch (error) {
         console.log('failed to connect to database!');
